94048e89dd0a8c2ec726a7c20b2efbfa088218b1
kent99821 <61462124+kent99821@users.noreply.github.com>
2021-05-04 11:29:45 +0800

    完成getRoomInfo云函数

--- cloudfunctions/getUserInfo/index.js
+++ cloudfunctions/getUserInfo/index.js
@@ -1,16 +1,36 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
-
 cloud.init()
-
+// 初始化数据库
+const db = cloud.database()
+// 页面数据
+let result 
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
+  switch (event.flag) {
+    case 0:
+      break;
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      result = await db.collection('users').add({
+        data: {
+          isAdmin: false,
+          isOver: true,
+          openId: wxContext.OPENID,
+          recordNum: 0,
+          roomAdminList: [],
+          sumTime: 0,
+          userName: event.userName
+        },
 
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
+      })
+      return result.errMsg
+
   }
+  
+
 }
--- miniprogram/pages/test/test.js
+++ miniprogram/pages/test/test.js
@@ -1,26 +1,60 @@
-
-
 Page({
 
   data: {
-  
+
   },
 
-
-  testFunction() {
+  // 调用getRoomInfo函数部分
+  //flag(number):0 查询多个自习室基础信息 roomIds(string array) 
+  getRooms() {
     wx.cloud.callFunction({
-      name: '',
+      name: 'getRoomInfo',
       data: {
-       
+        flag: 0,
+        roomIds: ['123456', '654321']
       },
       success: res => {
-        
+        console.log(res)
       },
       fail: err => {
         console.error('调用失败：', err)
       }
     })
   },
-
+  //flag(number):0 查询单个自习室具体信息 roomId(string) 
+  getRoom() {
+    wx.cloud.callFunction({
+      name: 'getRoomInfo',
+      data: {
+        flag: 1,
+        roomId: '123456'
+      },
+      success: res => {
+        console.log(res)
+      },
+      fail: err => {
+        console.log('调用失败：', err)
+      }
+    })
+  },
+  //调用getUserInfo部分
+addUser(){
+  // 创建用户档案前端需要传入参数：用户名 userName(string)
+    wx.cloud.callFunction({
+      name:'getUserInfo',
+      data:{
+      flag:3,
+      userName:"kent",
+      },
+      success:res=>{
+        console.log(res)
+      },
+      fail:err=>{
+        console.log('调用失败：',err)
+      }
+    })
+}
 })
+
+
 
