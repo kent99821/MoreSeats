419883fded39de9c1bb8f89c7b9950faa37a5e67
lafish <lafish@lafish.fun>
2021-05-06 19:45:29 +0800

    111

--- miniprogram/pages/adminRoomHistory/adminRoomHistory.js
+++ miniprogram/pages/adminRoomHistory/adminRoomHistory.js
@@ -1,66 +1,41 @@
-// pages/adminRoomHistory/adminRoomHistory.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+      visible1: false,
+      visible2: false,
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  open1() {
+      this.setData({
+          visible1: true,
+      })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  open2() {
+      this.setData({
+          visible2: true,
+      })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  close1() {
+      this.setData({
+          visible1: false,
+      })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  close2() {
+      this.setData({
+          visible2: false,
+      })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  onClose(key) {
+      console.log('onClose')
+      this.setData({
+          [key]: false,
+      })
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  onClose1() {
+      this.onClose('visible1')
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  onClose2() {
+      this.onClose('visible2')
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  onClosed1() {
+      console.log('onClosed')
+  },
 })
