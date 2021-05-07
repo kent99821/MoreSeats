f09ba31c8a95486998ab4915ebdda01324940447
kent99821 <61462124+kent99821@users.noreply.github.com>
2021-05-07 21:22:26 +0800

    初始化管理自习室功能

--- cloudfunctions/adminAction/index.js
+++ cloudfunctions/adminAction/index.js
@@ -1,21 +1,46 @@
+/**
+ * date:2021.05.06
+ * author:kent
+ * state:finishing
+ * content:create
+ */
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
-
 cloud.init()
-
-
 // 初始化数据库
 let db=wx.cloud.database()
-let rooms=db.collection('rooms')
+let PageData={
+reCode:0,//状态码
+result:{},//查询结果
+aresult:{},//添加结果
+croomId:"",//创建的roomid
+}
+function uuid() {
+  var chars = '0123456789'.split('');
+  var uuid = [], i;
+    for (i = 0; i < 6; i++) uuid[i] = chars[0 | Math.random()*10];
+  return uuid.join('');
+}
 
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
-
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
-  }
+      PageData.result=await db.collection('rooms').where({
+      roomId:"123456"
+    }).get()
+    return PageData.result
+// switch(event.flag){
+//   case 0:
+//     while(1){
+//      PageData.croomId=uuid()
+//     PageData.result=await db.collection('rooms').where({
+//       roomId:PageData.croomId
+//     }).get()
+//   if(PageData.result.data.length===0){
+//     // PageData.aresult=await db.collection('rooms').where({
+//     //   openId:event.openId
+//     // }).get()
+//   }
+//     }
+// }
 }
