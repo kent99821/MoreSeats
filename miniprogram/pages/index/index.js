812e1b852990e855dbf78d8d34afb75b49f7d24d
lafish <lafish@lafish.fun>
2021-05-07 21:24:24 +0800

    index接口加载

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -141,7 +141,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getHistory()
   },
 
   /**
@@ -155,7 +155,6 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    this.getHistory()
 
   },
 
