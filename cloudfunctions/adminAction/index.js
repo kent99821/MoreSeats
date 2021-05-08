83e2243a70dc61ad61c19b5906d345308780ecf8
kent99821 <61462124+kent99821@users.noreply.github.com>
2021-05-08 15:23:54 +0800

    完成修改自习室公告与开放时段功能

--- cloudfunctions/adminAction/index.js
+++ cloudfunctions/adminAction/index.js
@@ -15,7 +15,7 @@ let PageData = {
   aresult: {}, //操作后结果
   croomId: "", //创建的roomid
 }
-
+//获取六位随机码
 function uuid() {
   var chars = '0123456789'.split('');
   var uuid = [],
@@ -23,17 +23,74 @@ function uuid() {
   for (i = 0; i < 6; i++) uuid[i] = chars[0 | Math.random() * 10];
   return uuid.join('');
 }
+// 修改功能 自习室 公告 开放时段 
+async function update(event, flag) {
+  switch (flag) {
+    case 1:
+      PageData.result = await db.collection('rooms').where({
+        roomId: event.roomId
+      }).update({
+        data: {
+          roomName: event.roomName
+        }
+      })
+      break;
+    case 2:
+      PageData.result = await db.collection('rooms').where({
+        roomId: event.roomId
+      }).update({
+        data: {
+          roomNotice: event.roomNotice
+        }
+      })
+      break;
+    case 3:
+      PageData.result = await db.collection('rooms').where({
+        roomId: event.roomId
+      }).update({
+        data: {
+          openTime: event.openTime
+        }
+      })
+      break;
+  }
+  if (PageData.result.errMsg === "collection.update:ok") {
+    if (PageData.result.stats.updated === 0) {
+      PageData.reCode = 404
+      return {
+        "resCode": PageData.reCode,
+        "Msg": "自习室不存在",
+        "data": {}
+      }
+    } else if (PageData.result.stats.updated === 1) {
+      PageData.reCode = 200
+      return {
+        "resCode": PageData.reCode,
+        "Msg": "修改成功",
+        "data": {
+          roomId: event.roomId
+        }
+      }
+    } else {
+      PageData.reCode = 203
+      return {
+        "resCode": PageData.reCode,
+        "Msg": "警告:修改记录的数量不符",
+        "data": {}
+      }
+    }
 
-// room的记录格式
-// chairs: {infos:[], sitDown: 0, type: 0, chairNum: 0, group: []}
-// count: {pepSum: 0, rank: [], timeSum: 0}
-// isOpen: true
-// openId: ""
-// openTime: ""
-// roomId: ""
-// roomName: ""
-// roomNorice: {}
-// rule: {type:0 , size: 0, longtitude: 0, latitude: 0}
+  } else {
+    PageData.reCode = 405
+    return {
+      "resCode": PageData.reCode,
+      "Msg": "后台接口错误",
+      "data": {}
+    }
+  }
+}
+
+
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
@@ -64,7 +121,7 @@ exports.main = async (event, context) => {
               openTime: "",
               roomId: PageData.croomId,
               roomName: event.roomName,
-              roomNorice: {},
+              roomNotice: {},
               rule: {
                 type: 0,
                 size: 0,
@@ -79,8 +136,8 @@ exports.main = async (event, context) => {
               "resCode": PageData.reCode,
               "Msg": "创建成功",
               "data": {
-                roomId:PageData.croomId,
-                roomName:event.roomName
+                roomId: PageData.croomId,
+                roomName: event.roomName
               }
             }
           } else {
@@ -94,50 +151,12 @@ exports.main = async (event, context) => {
 
         }
       }
-    case 1:
-    PageData.result=await db.collection('rooms').where({
-      roomId:event.roomId
-    }).update({
-      data:{
-        roomName:event.roomName
-      }
-    })
-    if(PageData.result.errMsg==="collection.update:ok"){
-    if(PageData.result.stats.updated===0){
-      PageData.reCode=404
-      return {
-        "resCode":PageData.reCode,
-        "Msg":"自习室不存在",
-        "data":{}
-       }
-    }
-    else if(PageData.result.stats.updated===1){
-      PageData.reCode=200
-      return {
-        "resCode":PageData.reCode,
-        "Msg":"修改成功",
-        "data":{
-          roomId:event.roomId,
-          roomName:event.roomName
-        }
-       }
-    }
-    else{
-      PageData.reCode=203
-      return {
-        "resCode":PageData.reCode,
-        "Msg":"警告:修改记录的数量不符",
-        "data":{}
-       }
-    }
-    
-    }else{
-      PageData.reCode=405
-      return {
-        "resCode":PageData.reCode,
-        "Msg":"后台接口错误",
-        "data":{}
-       }
-    }
+      case 1:
+        return update(event, 1)
+      case 2:
+        return update(event, 2)
+      case 3:
+        return update(event, 3)
+
   }
 }
