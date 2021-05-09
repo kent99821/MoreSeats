688bb0df79930911bb11490fc4795d24635a3ab7
陈双龙 <1390158928@qq.com>
2021-05-09 12:26:07 +0800

    a

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -5,11 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-<<<<<<< HEAD
-=======
     showTips:true,
     newGuysORtoChair:true,
->>>>>>> master
     typeInVisible: false,
     rooms: [
       { "roomId": "122222", "roomName": "小黑屋屋屋屋屋习室1", openTime: "7 : 00 ~ 23 : 00", "chairNum": 50, "sitDown": 12 },
