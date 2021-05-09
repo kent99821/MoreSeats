6e83ee554bac58e98092a7233cf1bb0072cf1160
kent99821 <61462124+kent99821@users.noreply.github.com>
2021-05-09 20:28:50 +0800

    排行榜功能

---
+++ cloudfunctions/getRank/index.js
@@ -0,0 +1,26 @@
+/**
+ * date:2021.05.09
+ * author:kent
+ * state:finishing
+ * content:create
+ */
+
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+cloud.init()
+// 初始化数据库
+const db = cloud.database()
+const $ = db.command.aggregate
+let PageData={
+  resCode:0,
+  result:{}//查询结果
+}
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+  PageData.result=await db.collection('ranks').where({
+    roomId:event.roomId,
+  }).orderBy('uTimeSum', 'desc').skip(event.skip).limit(event.num).get()
+ return PageData.result
+
+}
