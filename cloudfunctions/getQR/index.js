413a056e7409252ca8082d530c402c00fca71a48
lafish <lafish@lafish.fun>
2021-05-14 13:08:08 +0800

    qr

--- cloudfunctions/getQR/index.js
+++ cloudfunctions/getQR/index.js
@@ -1,32 +1,58 @@
-// 云函数入口文件
 const cloud = require('wx-server-sdk')
+const JsZip = require('jszip')
 
-cloud.init()
+cloud.init({
+  env: 'cloud1-8gmb0klh8ea93cef'
+})
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
-  var scene, page
+
+  // event = {
+  //   flag,
+  //   chairNum,
+  //   indexStart,
+  //   roomId
+  // }
   switch (event.flag) {
     case 0:
-      // page = 'pages/chair/chair'
-      page = 'pages/index/index'
-      scene = `chairIndex=${event.chairIndex}`
-      break;
+      var Azip = JsZip()
+      let indexEnd = event.indexStart + event.chairNum
+      for (let index = event.indexStart; index < indexEnd; index++) {
+        console.log(index);
+        let result = await cloud.openapi.wxacode.getUnlimited({
+          page: 'pages/chair/chair',
+          scene: `chairIndex=${index}&roomId=${event.roomId}`,
+          lineColor: { "r": 255, "g": 121, "b": 91 },
+          width: 300
+        })
+        Azip.file(`${index + 1}.jpg`, result.buffer)
+      }
+      const zip = await Azip.generateAsync({
+        type: "nodebuffer",
+        compression: "DEFLATE",
+        compressionOptions: {
+          level: 1
+        }
+      })
+      const rFId = await cloud.uploadFile({
+        cloudPath: `qr/room-${event.roomId}_${event.indexStart + 1}-${indexEnd}.zip`,
+        fileContent: zip,
+      })
+
+      const link = await cloud.getTempFileURL({
+        fileList: [rFId.fileID],
+      })
+
+      return link.fileList[0]
     case 1:
-      page = 'pages/room/room'
-      scene = `roomId=${event.roomId}`
-      break;
-    case 2:
-      page = event.page
-      scene = event.scene
-      break;
-    default:
-      break;
+
+      return await cloud.openapi.wxacode.getUnlimited({
+        page: 'pages/room/room',
+        scene: `roomId=${event.roomId}`,
+        lineColor: { "r": 255, "g": 121, "b": 91 },
+        width: 420
+      })
   }
-  const result = await cloud.openapi.wxacode.getUnlimited({
-    scene,
-    page
-  })
-  return result
+
 }
