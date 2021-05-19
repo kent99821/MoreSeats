c5c1768976f9ab065b17252a753e6b60c50aa4e7
lafish <lafish@lafish.fun>
2021-05-19 13:07:29 +0800

    signin

--- miniprogram/pages/signIn/signIn.js
+++ miniprogram/pages/signIn/signIn.js
@@ -1,11 +1,13 @@
 // pages/signIn/signIn.js
+import { $wuxToptips } from '../../miniprogram_npm/wux-weapp/index.js'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    name: ""
   },
 
   /**
@@ -62,5 +64,51 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  ok: function (e) {
+    wx.showLoading({
+      title: '加载中',
+    })
+    let n = this.data.name.replace(/(^\s*)|(\s*$)/g, "")
+    if (n.length == 0 || n.length > 15) {
+      wx.hideLoading()
+      $wuxToptips().warn({
+        text: '长度限制1~15位字符',
+        duration: 2000
+      })
+      return
+    }
+    wx.cloud.callFunction({
+      name: 'getUserInfo',
+      data: {
+        flag: 3,
+        userName: n
+      },
+      success: res => {
+        console.log(res)
+        // console.log(res.result.resCode === 200)
+        if (res.result.resCode === 200) {
+          $wuxToptips().success({
+            text: '登记成功',
+            duration: 2000
+          })
+        } else {
+          $wuxToptips().warn({
+            text: '重复登记或登记失败',
+            duration: 2000
+          })
+        }
+        setTimeout(function () {
+          wx.navigateBack()
+        }, 1000)
+      },
+      fail: (res) => {
+        wx.showToast({
+          title: '云开发出现了些问题，请联系管理员排查！',
+          icon: "none"
+        })
+        console.log(res);
+      }
+    })
   }
 })
